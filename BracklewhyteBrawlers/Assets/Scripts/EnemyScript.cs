8c626ed3d0716d498ea7556cb003230744a277af
jojopak <pak.j.jedi@gmail.com>
2020-02-13 18:35:31 -0800

    Adding raw unity folders

---
+++ BracklewhyteBrawlers/Assets/Scripts/EnemyScript.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyScript : MonoBehaviour
+{
+
+    private GameObject player;
+    private NavMeshAgent agent;
+    private Rigidbody enemyRb;
+    public float speed = 3f;
+
+    void Start()
+    {
+
+        player = GameObject.Find("Player");
+        agent = GetComponent<NavMeshAgent>();
+        enemyRb = GetComponent<Rigidbody>();
+        agent.speed = speed;
+    }
+
+    void Update()
+    {
+        //agent.SetDestination(player.transform.position);
+        Vector3 lookDirection = (player.transform.position - transform.position);//.normalized;
+
+        enemyRb.AddForce(lookDirection * speed);
+
+
+    }
+}
---
+++ BracklewhyteBrawlers/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public bool inWindZone = false;
+    public GameObject windZone;
+    public float speed = 5f;
+    public float mass = 3f;
+    public float size = 1;
+    public Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.mass = mass;
+    }
+
+    private void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal * speed * mass, 0.0f, moveVertical * speed * mass);
+        rb.AddForce(movement);
+
+        if (inWindZone)
+        {
+            rb.AddForce(windZone.GetComponent<WindArea>().direction * windZone.GetComponent<WindArea>().strength);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider coll)
+    {
+        if (coll.gameObject.tag == "WindArea")
+        {
+            windZone = coll.gameObject;
+            inWindZone = true;
+        }
+
+        if (coll.gameObject.CompareTag("Pick Up"))
+        {
+            coll.gameObject.SetActive(false);
+            mass += 2;
+            rb.mass = mass;
+
+        }
+    }
+    void OnTriggerExit(Collider coll)
+    {
+        if (coll.gameObject.tag == "WindArea")
+        {
+            inWindZone = false;
+        }
+    }
+}
